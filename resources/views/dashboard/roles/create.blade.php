95acecdd4833f3b0429f4f983fc78c2f6049818b
gamal-elhalwany <gamalelhalwany3@gmail.com>
2024-12-28 00:58:44 +0200

    Modified Role Create Page.

--- resources/views/dashboard/roles/create.blade.php
+++ resources/views/dashboard/roles/create.blade.php
@@ -7,20 +7,16 @@
 
     <div class="body">
 
-        <div class="create-role-container">
+        <div class="create-role-container container mt-5">
 
             <div class="create-role-head">
-                <a class="btn btn-info" href="{{route('dashboard.roles.index')}}">
+                <a class="btn btn-info mb-3" href="{{route('dashboard.roles.index')}}">
                     <i style="vertical-align: middle" class="fa-solid fa-backward"></i>
                     رجوع
                 </a>
                 <h4>انشاء صلاحية جديدة</h4>
 
             </div>
-
-
-
-
             <div class="create-role-content">
                 @if (count($errors) > 0)
                     <div class="alert alert-danger">
@@ -32,8 +28,8 @@
                     </div>
                 @endif
                 {!! Form::open(array('route' => 'dashboard.roles.store','method'=>'POST')) !!}
-                    <div class="input-style role-name">
-                        <strong>أسم الصحلاية:</strong>
+                    <div class="input-style role-name m-4">
+                        <strong>أسم الصلاحية:</strong>
                         {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
                     </div>
                     <div class="input-style checkBox">
